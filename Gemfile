dd3525d9c336fa53f4d37424e31c496b65a37c6c
Travis Ueki <travis.ueki@gmail.com>
2014-09-08 03:55:44 -0700

    implemented devise for users

--- Gemfile
+++ Gemfile
@@ -7,9 +7,12 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
-gem 'bootstrap-sass'         
+gem 'bootstrap-sass' 
+gem 'devise'        
 
-
+group :develpment, :test do
+	gem 'sqlite3'
+end
 
 group :production do
 	gem 'pg'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+   #required for heroku
+   #note to set this to your actual host
+  config.action_mailer.default_url_options = { host: 'task3r.us', port: 3000 }
+
 end
